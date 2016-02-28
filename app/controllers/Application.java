db4bbc7930fd36acea900d3392d52d49d2e720ab
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2016-02-27 23:08:24 -0800

    CA serves decryprion keys and public key

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,8 +2,11 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.data.DynamicForm;
+import java.util.*;
 import views.html.*;
+import cpabe.Cpabe;
+import java.io.PrintWriter;
 
 public class Application extends Controller {
 
@@ -11,4 +14,22 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public Result register() {
+        final Map<String, String[]> values = request().body().asFormUrlEncoded();
+        final String name = values.get("name")[0];
+        String attr_list = values.get("attr_list")[0];
+        String pubfile = "./public/keys/pub_key";
+        String mskfile = "./public/keys/master_key";
+        String prvfile = "./public/keys/prv_key";
+
+        Cpabe test = new Cpabe();
+
+        try {
+            test.setup(pubfile, mskfile);
+            test.keygen(pubfile, prvfile, mskfile, attr_list);
+        }catch (Exception e) {
+
+        }
+        return ok(keys.render());
+    }
 }
